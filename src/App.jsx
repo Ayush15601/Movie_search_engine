0e4279638832d54fe8c34acd2f18a1b001aa693f
Ayush15601 <aayushtewarit@gmail.com>
2026-05-22 09:33:49 +0530

    added routing feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Home from './assets/pages/home'
+import {Routes, Route } from 'react-router-dom';
+import Favourites from './assets/pages/favourite';
 
 
 function App() {
     
   return (
     <>
-
-    <Home />
-    
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/favourites' element={<Favourites />}/>
+      </Routes>
+    </main>    
     </>
   )
 }

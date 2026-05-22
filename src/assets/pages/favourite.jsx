557527fb68bf09d715540a049f8739f4712a5770
Ayush15601 <aayushtewarit@gmail.com>
2026-05-22 09:24:47 +0530

    added favourite components

---
+++ src/assets/pages/favourite.jsx
@@ -0,0 +1,12 @@
+function Favourites() {
+    return (
+        <>
+        <div className="favourites-empty">
+            <h2> NO fav movies yet</h2>
+            <p> Add it fast</p>
+        </div>   
+        </>
+    )
+}
+
+export default Favourites  
